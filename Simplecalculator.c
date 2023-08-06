e7ddc000ea9d5e7030be1e5b69462e1e9ba10eb3
Vikas Reddy <42316850+vikasrex@users.noreply.github.com>
2023-08-06 17:03:03 +0530

    Simplecalculator.c

--- Simplecalculator.c
+++ Simplecalculator.c
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float value1,value2;
+    char operator;
+    float answer;
+    printf("Enter calculation:\n");
+    scanf("%f%c%f",&value1,&operator,&value2);
+    switch(operator)
+    {
+        case '/': answer = value1/value2;
+        break;
+        case '*': answer = value1*value2;
+        break;
+        case '+': answer = value1+value2;
+        break;
+        case '-': answer = value1-value2;
+        break;
+        default : printf("Fail.\n");
+    }   printf("%.9g%c%.9g=%.6g\n\n",value1,operator, value2, answer);
+}
